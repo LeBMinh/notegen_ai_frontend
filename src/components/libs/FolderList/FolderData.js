eea49f10fc317931d1e4a42ad65ae999fbb73f5a
LeBMinh <155410101+LeBMinh@users.noreply.github.com>
2025-02-02 13:40:55 +0700

    main page

--- src/components/libs/FolderList/FolderData.js
+++ src/components/libs/FolderList/FolderData.js
@@ -3,93 +3,93 @@ const folders = [
       id: "folder-1", // Unique folder ID
       title: "English Language Class",
       notes: [
-        { id: "note-1", title: "Grammar Basics", content: "Introduction to verbs, nouns, and tenses." },
-        { id: "note-2", title: "Advanced Vocabulary", content: "Explore complex words and their usages." },
-        { id: "note-3", title: "Essay Writing", content: "Learn to write impactful essays." }
+        { id: "note_1", title: "Grammar Basics", content: "Introduction to verbs, nouns, and tenses." },
+        { id: "note_2", title: "Advanced Vocabulary", content: "Explore complex words and their usages." },
+        { id: "note_3", title: "Essay Writing", content: "Learn to write impactful essays." }
       ]
     },
     {
       id: "folder-2",
       title: "Art Renaissance to Modern",
       notes: [
-        { id: "note-4", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
-        { id: "note-5", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
+        { id: "note_4", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
+        { id: "note_5", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
       ]
     },
     {
       id: "folder-3",
       title: "Art Renaissance to Modern",
       notes: [
-        { id: "note-6", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
-        { id: "note-7", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
+        { id: "note_6", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
+        { id: "note_7", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
       ]
     },
     {
       id: "folder-4",
       title: "Art Renaissance to Modern",
       notes: [
-        { id: "note-8", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
-        { id: "note-9", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." },
-        { id: "note-10", title: "Impressionism", content: "Analyzing the impact of Impressionist painters. Analyzing the impact of Impressionist painters. Analyzing the impact of Impressionist painters. Analyzing the impact of Impressionist painters." },
-        { id: "note-11", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
-        { id: "note-12", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
-        { id: "note-13", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." },
+        { id: "note_8", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
+        { id: "note_9", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." },
+        { id: "note_10", title: "Impressionism", content: "Analyzing the impact of Impressionist painters. Analyzing the impact of Impressionist painters. Analyzing the impact of Impressionist painters. Analyzing the impact of Impressionist painters." },
+        { id: "note_11", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
+        { id: "note_12", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
+        { id: "note_13", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." },
       ]
     },
     {
       id: "folder-5",
       title: "Art Renaissance to Modern",
       notes: [
-        { id: "note-14", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
-        { id: "note-15", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
+        { id: "note_14", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
+        { id: "note_15", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
       ]
     },
     {
       id: "folder-6",
       title: "Art Renaissance to Modern",
       notes: [
-        { id: "note-16", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
-        { id: "note-17", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
+        { id: "note_16", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
+        { id: "note_17", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
       ]
     },
     {
       id: "folder-7",
       title: "Art Renaissance to Modern",
       notes: [
-        { id: "note-18", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
-        { id: "note-19", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
+        { id: "note_18", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
+        { id: "note_19", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
       ]
     },
     {
       id: "folder-7",
       title: "Art Renaissance to Modern",
       notes: [
-        { id: "note-20", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
-        { id: "note-21", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
+        { id: "note_20", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
+        { id: "note_21", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
       ]
     },
     {
       id: "folder-8",
       title: "Art Renaissance to Modern",
       notes: [
-        { id: "note-22", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
-        { id: "note-23", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
+        { id: "note_22", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
+        { id: "note_23", title: "Impressionism", content: "Analyzing the impact of Impressionist painters." }
       ]
     },
     {
       id: "folder-9",
       title: "Art Renaissance to Modern",
       notes: [
-        { id: "note-24", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
-        { id: "note-25", title: "History of Vietnam culture", content: "Analyzing the impact of Impressionist painters." }
+        { id: "note_24", title: "The Renaissance Era", content: "Exploring art and culture in Europe during the 14th century." },
+        { id: "note_25", title: "History of Vietnam culture", content: "Analyzing the impact of Impressionist painters." }
       ]
     },
     {
       id: "folder-10",
       title: "History of Vietnam Culture",
       notes: [
-        { id: "note-26", title: "Ancient Vietnam", content: "Understanding cultural roots and traditions." },
-        { id: "note-27", title: "Colonial Influence", content: "Impact of French colonization on Vietnamese culture." }
+        { id: "note_26", title: "Ancient Vietnam", content: "Understanding cultural roots and traditions." },
+        { id: "note_27", title: "Colonial Influence", content: "Impact of French colonization on Vietnamese culture." }
       ]
     }
   ];
--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import './Dashboard.css';
 // import folder and note list
 import folders from "../../libs/FolderList/FolderData";
@@ -13,9 +13,61 @@ import LgGradientNoteNow from '../../../assets/Icon_line-Gradient/GrabYourNote_L
 
 export default function Dashboard() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [pinnedNotes, setPinnedNotes] = useState([]);
 
   // Flatten notes from folders
   const notes = folders.flatMap((folder) => folder.notes);
+
+  const handlePinNote = (note) => {
+    setPinnedNotes((prevPinnedNotes) => {
+      if (prevPinnedNotes.find((pinnedNote) => pinnedNote.id === note.id)) {
+        // If already pinned, unpin it
+        return prevPinnedNotes.filter((pinnedNote) => pinnedNote.id !== note.id);
+      } else {
+        // Pin the note
+        return [...prevPinnedNotes, note];
+      }
+    });
+  };
+
+  // const filteredNotes = [
+  //   ...pinnedNotes.filter(
+  //     (note) =>
+  //       note.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+  //       note.content.toLowerCase().includes(searchTerm.toLowerCase())
+  //   ),
+  //   ...notes
+  //     .filter(
+  //       (note) =>
+  //         !pinnedNotes.find((pinnedNote) => pinnedNote.id === note.id) && // Exclude pinned notes
+  //         (note.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+  //           note.content.toLowerCase().includes(searchTerm.toLowerCase()))
+  //     ),
+  // ];
+
+  const filteredNotes = useMemo(() => {
+    return [
+      ...pinnedNotes.filter(
+        (note) =>
+          note.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          note.content.toLowerCase().includes(searchTerm.toLowerCase())
+      ),
+      ...notes.filter(
+        (note) =>
+          !pinnedNotes.find((pinnedNote) => pinnedNote.id === note.id) &&
+          (note.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            note.content.toLowerCase().includes(searchTerm.toLowerCase()))
+      ),
+    ];
+  }, [searchTerm, pinnedNotes, notes]);
+  
+
+  const highlightText = (text, searchTerm) => {
+    if (!searchTerm) return text;  // No search term, return original text
+    const regex = new RegExp(`(${searchTerm})`, "gi");
+    return text.replace(regex, (match) => `<span class="highlight">${match}</span>`);
+  };
+
 
   return (
     <div className='dashboard-container'>
@@ -79,21 +131,41 @@ export default function Dashboard() {
 
         {/* Note List */}
         <div className="dashboard-note-cards-container">
-          {notes.map((note) => (
-            <div key={note.id} className="dashboard-note-card">
-              <span className="dashboard-note-time">
-                <img src={EditTimeAgo} alt="Time Icon" className="dashboard-note-timeAgo-icon"/>
-                 2 days ago
-              </span>
-              <img src={Pin} alt="Pin Icon" className="dashboard-note-pin-icon" />
-              <div className="dashboard-note-TandC">
-                <h3 className="dashboard-note-title">{note.title}</h3>
-                <p className="dashboard-note-content">
-                  {note.content.length > 70 ? `${note.content.slice(0, 70)}...` : note.content}
-                </p>
+          {filteredNotes.length > 0 ? (
+            filteredNotes.map((note) => (
+              <div key={note.id} className="dashboard-note-card">
+                <span className="dashboard-note-time">
+                  <img src={EditTimeAgo} alt="Time Icon" className="dashboard-note-timeAgo-icon" />
+                  2 days ago
+                </span>
+                {/* <img
+                  src={Pin}
+                  alt="Pin Icon"
+                  className="dashboard-note-pin-icon"
+                  onClick={() => handlePinNote(note)} // Attach click event
+                /> */}
+                <div className="dashboard-note-TandC">
+                  <h3 className="dashboard-note-title">{note.title}</h3>
+                  <p
+                    className="dashboard-note-content"
+                    dangerouslySetInnerHTML={{
+                      __html: highlightText(
+                        note.content.length > 70
+                          ? `${note.content.slice(0, 70)}...`
+                          : note.content,
+                        searchTerm
+                      ),
+                    }}
+                  />
+                </div>
               </div>
+            ))
+          ) : (
+            <div className="empty-search-found">
+              No notes found for "{searchTerm}"
             </div>
-          ))}
+          )}
+
         </div>
       </div>
 
